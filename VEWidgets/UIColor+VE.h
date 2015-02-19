c661bb1e801c521832eaa4e76883b6706eefa32d
加帆 <jiafan.zhang@qq.com>
2015-02-19 15:26:36 +0800

    UIColor 扩展： 1、支持16进制颜色值转换

---
+++ VEWidgets/UIColor+VE.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+VE.h
+//  VisualEditWidgets
+//
+//  Created by 加帆 on 2/19/15.
+//  Copyright (c) 2015 jiafan.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (VE)
++ (UIColor *)colorWithHex:(NSString *)hex alpha:(float)alpha;
+@end
---
+++ VEWidgets/UIColor+VE.m
@@ -0,0 +1,44 @@
+//
+//  UIColor+VE.m
+//  VisualEditWidgets
+//
+//  Created by 加帆 on 2/19/15.
+//  Copyright (c) 2015 jiafan.net. All rights reserved.
+//
+
+#import "UIColor+VE.h"
+
+@implementation UIColor (VE)
++ (UIColor *)colorWithHex:(NSString *)hex alpha:(float)alpha
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    // String should be 6 or 8 characters
+    
+    if ([cString length] < 6) return [UIColor blackColor];
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    if ([cString hasPrefix:@"#"]) cString = [cString substringFromIndex:1];
+    if ([cString length] != 6) return [UIColor blackColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    // Scan values
+    unsigned int r, g, b;
+    
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+@end
