c2e1acbda8bae5accc02d1e8436ba52b81abbed2
加帆 <jiafan.zhang@qq.com>
2015-04-20 17:08:48 +0800

    修改 16进制 颜色获取方法

--- VEWidgets/UIColor+VE.m
+++ VEWidgets/UIColor+VE.m
@@ -39,6 +39,6 @@
     return [UIColor colorWithRed:((float) r / 255.0f)
                            green:((float) g / 255.0f)
                             blue:((float) b / 255.0f)
-                           alpha:1.0f];
+                           alpha:alpha];
 }
 @end
